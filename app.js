fb844e07339a5eb62bfb4a164906bec49e320e55
weideow <owweide@hotmail.com>
2025-01-02 00:03:33 +0800

    color buttons

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ board = ["","",""];
 // board1 = ["","",""];
 winner = false;
 loss = false;
-colorChoice = ['green','red','blue'];
+
 
 const playerChoice = document.querySelectorAll('.column');
 // const playerChoice1 = document.querySelectorAll('.column1');
@@ -15,11 +15,6 @@ function render() {
 
 };
 
-function updateBoard() {
-    playerChoice.forEach((Choice, index) =>{
-    Choice.innerText = board[index];
-    });
-}
 
 // function updateBoard1() {
 //     playerChoice1.forEach((Choice1, index) =>{
@@ -32,14 +27,78 @@ function handleClick(event) {
     placePiece (squareIndex);
 }
 
-const sect = document.querySelector('section');
-const btn = document.querySelector('.column');
+// const colorChoice = ['green', 'red', 'blue']; 
+// const sect = document.querySelector('section');
+// const columnBtns = document.querySelectorAll('.column');
 
-let currentColorIndex = -1;
+// let currentColorIndex = -1;
 
-btn.addEventListener('click', () =>{
-    const nextColorIndex = ++currentColorIndex % colorChoice.length;
-    sect.style.color = colorChoice[nextColorIndex];
+
+// columnBtns.forEach((btn) => {
+//     btn.addEventListener('click', () => {
+
+//         const nextColorIndex = ++currentColorIndex % colorChoice.length;
+//         sect.style.backgroundColor = colorChoice[nextColorIndex]; 
+//     });
+// });this is wrong, color was applied to the background instead of buttons itself
+
+const colorChoice = ['green', 'red', 'blue']; 
+const columnBtns = document.querySelectorAll('.column');
+const hiddenAns = document.querySelectorAll('.sqr');
+
+let currentColorIndex = 0;
+
+columnBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        const nextColorIndex = ++currentColorIndex % colorChoice.length; //colorChoice is 0,1,2. ++ brings the loop through with 1. currentColorIndex is -1, upon 1st click, it will be 0.
+        //0%3 gives 0, and so on. once it goes to 3%3, result will be 0, meaning the loop resets.
+        btn.style.backgroundColor = colorChoice[nextColorIndex];  
+    });
 });
+// https://stackoverflow.com/questions/69995256/loop-through-colors/69995422#69995422 credits to stackoverflow
+
+// function randomComp() {
+//     const compSelection = [];
+//     const colorChoice = ['green', 'red', 'blue'];
+//     for (let i =0; i<colorChoice.length; i++) {
+//         const randomSelect = Math.random(colorChoice);
+//         compSelection.push(randomSelect);
+//     }
+    
+// }//looped through but no selection
+
+
+
+
+// function randomComp() {
+//     const compSelection = [];
+//     const colorChoice = ['green', 'red', 'blue'];
+//     for (let i =0; i<colorChoice.length; i++) {
+//         const randomIndex = Math.floor(Math.random(colorChoice)*colorChoice.length);
+//         compSelection.push(colorChoice[randomIndex]);
+//     }
+//     return compSelection;
+// }
+
+function randomComp() {
+    const compSelection = [];
+    const colorChoice = ['green', 'red', 'blue'];
+    for (let i =0; i<colorChoice.length; i++) {
+        const randomIndex = Math.floor(Math.random()*colorChoice.length);
+        compSelection.push(colorChoice[randomIndex]);
+    }
+    return compSelection;
+}
+
+
+function updateBoard() {
+    hiddenAns.forEach((square) => {
+        for (let i = 0; i<compSelection.length; i++) {
+            square.style.backgroundColor = compSelection[i];
+        }
+
+   });
+}
+
 
 
