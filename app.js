c9553040d9ec1f2d52a05fe454a2ca677f713340
weideow <owweide@hotmail.com>
2025-01-06 06:51:18 +0800

    add rows

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function init () {
     render();
     randomComp();
     updateBoard();
-    // checkWinner1();
+    // checkWinner();
     // updateMessage();
     
 };
@@ -46,9 +46,12 @@ const columnBtns = document.querySelectorAll('.column');
 const hiddenAns = document.querySelectorAll('.sqr');
 const colorChoice = ['green', 'red', 'blue']; 
 const playerHint0 = document.querySelector('#ans0');
+const playerHint1 = document.querySelector('#ans1');
+const playerHint2 = document.querySelector('#ans2');
+const playerHint3 = document.querySelector('#ans3');
 
 let currentColorIndex = -1;
-let playerChoice ={key4:"", key5:"", key6:"", key7:"", key8:"", key9:""};
+let playerChoice ={key4:"", key5:"", key6:"", key7:"", key8:"", key9:"", key10:"", key11:"", key12:"", key13:"", key14:"", key15:""};
 
 columnBtns.forEach((btn, index) => {
     btn.addEventListener('click', () => {
@@ -67,28 +70,107 @@ columnBtns.forEach((btn, index) => {
 const confirmSelection0 = document.querySelector('#confirm0');
 confirmSelection0.addEventListener('click', () => {
     // checkAnswer1();
+    checkWinner0();
+    updateMessage();
+});
+
+const confirmSelection1 = document.querySelector('#confirm1');
+confirmSelection1.addEventListener('click', () => {
+    // checkAnswer1();
     checkWinner1();
     updateMessage();
 });
 
-const matchingValues = [];
-function checkAnswer1() {  //compare compSelection against playerChoice number 1
+const confirmSelection2 = document.querySelector('#confirm2');
+confirmSelection2.addEventListener('click', () => {
+    // checkAnswer1();
+    checkWinner2();
+    updateMessage();
+});
+
+const confirmSelection3 = document.querySelector('#confirm3');
+confirmSelection3.addEventListener('click', () => {
+    // checkAnswer1();
+    checkWinner3();
+    updateMessage();
+});
+
+const matchingValues0 = [];
+function checkAnswer0() {  //compare compSelection against playerChoice number 1
+        
+        if (compSelection.key0 === playerChoice.key4) {
+            matchingValues0.push(compSelection.key0);
+        }
+        if (compSelection.key1 === playerChoice.key5) {
+            matchingValues0.push(compSelection.key1);
+        }
+
+        if (compSelection.key2 === playerChoice.key6) {
+            matchingValues0.push(compSelection.key2);
+        }
+        
+        console.log(matchingValues0);
+        return matchingValues0;
+
+}
+
+const matchingValues1 = [];
+function checkAnswer1() { 
         
         if (compSelection.key0 === playerChoice.key7) {
-            matchingValues.push(compSelection.key0);
+            matchingValues1.push(compSelection.key0);
         }
         if (compSelection.key1 === playerChoice.key8) {
-            matchingValues.push(compSelection.key1);
+            matchingValues1.push(compSelection.key1);
         }
 
         if (compSelection.key2 === playerChoice.key9) {
-            matchingValues.push(compSelection.key2);
+            matchingValues1.push(compSelection.key2);
         }
         
-        console.log(matchingValues);
-        return matchingValues;
+        console.log(matchingValues1);
+        return matchingValues1;
 
 }
+
+const matchingValues2 = [];
+function checkAnswer2() { 
+        
+        if (compSelection.key0 === playerChoice.key10) {
+            matchingValues2.push(compSelection.key0);
+        }
+        if (compSelection.key1 === playerChoice.key11) {
+            matchingValues2.push(compSelection.key1);
+        }
+
+        if (compSelection.key2 === playerChoice.key12) {
+            matchingValues2.push(compSelection.key2);
+        }
+        
+        console.log(matchingValues2);
+        return matchingValues2;
+
+}
+
+const matchingValues3 = [];
+function checkAnswer3() { 
+        
+        if (compSelection.key0 === playerChoice.key13) {
+            matchingValues3.push(compSelection.key0);
+        }
+        if (compSelection.key1 === playerChoice.key14) {
+            matchingValues3.push(compSelection.key1);
+        }
+
+        if (compSelection.key2 === playerChoice.key15) {
+            matchingValues3.push(compSelection.key2);
+        }
+        
+        console.log(matchingValues3);
+        return matchingValues3;
+
+}
+
 
 function updateMessage() {
     if (winner === true){
@@ -99,22 +181,79 @@ function updateMessage() {
     }
 }
 
-function checkWinner1() {
-    checkAnswer1();
-    if (matchingValues.length === 3) {
+function checkWinner0() {
+    checkAnswer0();
+    if (matchingValues0.length === 3) {
         winner = true;
     }
 
-    else if (matchingValues.length === 2) {
+    else if (matchingValues0.length === 2) {
         playerHint0.textContent = "2 right values!"
     }
 
-    else if (matchingValues.length === 1) {
+    else if (matchingValues0.length === 1) {
         playerHint0.textContent = "1 right value!"
     }
 
-    else if (matchingValues.length === 0) {
+    else if (matchingValues0.length === 0) {
         playerHint0.textContent = " No correct value!"
+    }
+}
+
+function checkWinner1() {
+    checkAnswer1();
+    if (matchingValues1.length === 3) {
+        winner = true;
+    }
+
+    else if (matchingValues1.length === 2) {
+        playerHint1.textContent = "2 right values!"
+    }
+
+    else if (matchingValues1.length === 1) {
+        playerHint1.textContent = "1 right value!"
+    }
+
+    else if (matchingValues1.length === 0) {
+        playerHint1.textContent = " No correct value!"
+    }
+}
+
+function checkWinner2() {
+    checkAnswer2();
+    if (matchingValues2.length === 3) {
+        winner = true;
+    }
+
+    else if (matchingValues2.length === 2) {
+        playerHint2.textContent = "2 right values!"
+    }
+
+    else if (matchingValues2.length === 1) {
+        playerHint2.textContent = "1 right value!"
+    }
+
+    else if (matchingValues2.length === 0) {
+        playerHint2.textContent = " No correct value!"
+    }
+}
+
+function checkWinner3() {
+    checkAnswer3();
+    if (matchingValues3.length === 3) {
+        winner = true;
+    }
+
+    else if (matchingValues3.length === 2) {
+        playerHint3.textContent = "2 right values!"
+    }
+
+    else if (matchingValues3.length === 1) {
+        playerHint3.textContent = "1 right value!"
+    }
+
+    else if (matchingValues3.length === 0) {
+        playerHint3.textContent = " No correct value!"
     }
 }
 
